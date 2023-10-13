5b79e02f0d1e89b1a0c06554b62792ed7b11b2ba
Razin Syakib <muhammadrazin14@gmail.com>
2023-10-14 04:02:27 +0700

    Update 'findAllBooks' Controller
    Revised the 'findAllBooks' controller in 'booksController.js' to enhance code readability and maintainability when retrieving all books from the database using Sequelize's 'findAll' method, sourced from the 'Book' model.
    
    Result (Postman):
        "data": [
            {
                "id": 1,
                "title": "Node JS",
                "description": "Learning Node JS for beginner to advanced",
                "createdAt": "2023-10-13T20:43:57.000Z",
                "updatedAt": "2023-10-13T20:43:57.000Z"
            }
        ]

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -1,30 +1,20 @@
-const books = [
-  {
-    id: 1,
-    title: "Clean Code",
-    description: "A Handbook of Agile Software Craftsmanship",
-  },
-  {
-    id: 2,
-    title: "Code Complete",
-    description: "A Practical Handbook of Software Construction",
-  },
-  {
-    id: 3,
-    title: "Design Patterns",
-    description: "Elements of Reusable Object-Oriented Software",
-  },
-];
+const { Book } = require("../models");
 
 // get all books
-const findAllBooks = (req, res) => {
-  res.status(200).json({
-    code: 200,
-    status: "success",
-    error: false,
-    message: "Books retrieved successfully",
-    data: books,
-  });
+const findAllBooks = async (req, res) => {
+  try {
+    const data = await Book.findAll();
+
+    res.status(200).json({
+      code: 200,
+      status: "success",
+      error: false,
+      message: "Books retrieved successfully",
+      data: data,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 // get book by id
