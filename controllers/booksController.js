278bfaec4642dbd2b858bc74256f2107f19c9dfd
Razin Syakib <muhammadrazin14@gmail.com>
2023-10-15 04:31:30 +0700

    Create 'updateBookById' Controller
    Added the 'updateBookById' controller in the latest version to handle updating book information by its ID using the 'Book' model. Utilized Sequelize's 'save()' method to persist the changes to the database.
    
    Result (Postman):
    {
        "code": 200,
        "status": "success",
        "error": false,
        "message": "Book updated successfully",
        "data": {
            "id": "3",
            "title": "Learn MySQL",
            "description": "Learning MySQL for beginner to advanced",
            "createdAt": "2023-10-14T21:26:24.000Z",
            "updatedAt": "2023-10-14T21:28:39.485Z"
        }
    }

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -19,34 +19,34 @@ const findAllBooks = async (req, res) => {
 
 // get book by id
 const getBookById = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const book = await Book.findByPk(id);
+  try {
+    const { id } = req.params;
+    const book = await Book.findByPk(id);
 
-        if (!book) {
-          res.status(404).json({
-            code: 404,
-            status: "error",
-            error: true,
-            message: `Book with id ${id} not found`,
-          });
-        } else {
-          res.status(200).json({
-            code: 200,
-            status: "success",
-            error: false,
-            message: "Book retrieved successfully",
-            data: book,
-          });
-        }
-    } catch (error) {
-        console.log(error, "<- error getBookById");
+    if (!book) {
+      res.status(404).json({
+        code: 404,
+        status: "error",
+        error: true,
+        message: `Book with id ${id} not found`,
+      });
+    } else {
+      res.status(200).json({
+        code: 200,
+        status: "success",
+        error: false,
+        message: "Book retrieved successfully",
+        data: book,
+      });
     }
+  } catch (error) {
+    console.log(error, "<- error getBookById");
+  }
 };
 
 // post new book
 const createNewBook = async (req, res) => {
-  try{
+  try {
     const { title, description } = req.body;
     const newBook = await Book.create({
       title: title,
@@ -71,8 +71,56 @@ const createNewBook = async (req, res) => {
   }
 };
 
+// update book by id
+const updateBookById = async (req, res) => {
+  try {
+    // get book id from params
+    const { id } = req.params;
+    // get title and description from body
+    const { title, description } = req.body;
+    // find book by id from params
+    const book = await Book.findByPk(id);
+
+    // if book not found or found
+    if (!book) {
+      res.status(404).json({
+        code: 404,
+        status: "error",
+        error: true,
+        message: `Book with id ${id} not found`,
+      });
+    }
+
+    // update book data from body 
+    book.title = title;
+    book.description = description;
+    book.updatedAt = new Date();
+
+    // save book data to database
+    await book.save();
+
+    // response updated book
+    res.status(200).json({
+      code: 200,
+      status: "success",
+      error: false,
+      message: "Book updated successfully",
+      data: {
+        id: id,
+        title: title,
+        description: description,
+        createdAt: book.createdAt,
+        updatedAt: book.updatedAt,
+      },
+    });
+  } catch (error) {
+    console.log(error, "<- error updateBookById");
+  }
+};
+
 module.exports = {
   findAllBooks,
   getBookById,
   createNewBook,
+  updateBookById,
 };
--- routes/booksRoutes.js
+++ routes/booksRoutes.js
@@ -3,11 +3,13 @@ const {
   findAllBooks,
   getBookById,
   createNewBook,
+  updateBookById,
 } = require("../controllers/booksController");
 const router = express.Router();
 
 router.get("/books", findAllBooks);
 router.get("/books/:id", getBookById);
 router.post("/books", createNewBook);
+router.patch("/books/:id", updateBookById);
 
 module.exports = router;
